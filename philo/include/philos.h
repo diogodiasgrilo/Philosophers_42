7dc0b3b937e8a509d2d7fce1f220228a2924b419
Diogo Dias <diogodiasgrilo@gmail.com>
2023-05-14 19:21:20 +0100

    Add files via upload

--- philo/include/philos.h
+++ philo/include/philos.h
@@ -6,7 +6,7 @@
 /*   By: diogpere <diogpere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 16:25:23 by diogpere          #+#    #+#             */
-/*   Updated: 2023/05/12 16:27:10 by diogpere         ###   ########.fr       */
+/*   Updated: 2023/05/14 17:14:01 by diogpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,62 +19,66 @@
 # include <time.h>
 # include <pthread.h>
 # include <sys/time.h>
-# include "../libft/libft.h"
 
 struct	s_prog;
 
 typedef struct s_philo
 {
-	int				id;
-	int				last_meal;
-	int				t_counter;
-	int				dead;
-	int				times_eaten;
 	char			*num;
 	char			*eating;
 	char			*sleeping;
 	char			*thinking;
 	char			*died;
+	int				id;
+	int				dead;
 	int				n_phi;
 	int				t_m_eat;
+	int				last_meal;
 	pthread_t		th;
+	struct s_prog	*p;
 	pthread_mutex_t	*writing;
 	pthread_mutex_t	*l_fork;
 	pthread_mutex_t	*r_fork;
-	struct s_prog	*p;
 }				t_philo;
 
 typedef struct s_prog
 {
-	int				i;
 	t_philo			**philos;
-	pthread_mutex_t	**forks;
 	char			**argv;
-	int				begin;
+	int				i;
 	int				argc;
 	int				n_phi;
 	int				t_die;
 	int				t_eat;
 	int				t_sleep;
 	int				t_m_eat;
+	int				t_all_eaten;
+	pthread_mutex_t	**forks;
 	pthread_mutex_t	writing;
-	int				elapsed_time;
 	struct timeval	start_time;
 	struct timeval	current_time;
 }				t_prog;
 
-void	get_time(t_prog	*p);
-int		find_death(t_prog *p);
-void	*routine(void *philo_v);
-void	*build_params(t_prog *p);
-void	start_threads(t_prog *p);
-void	*small_r(t_philo *philo);
-void	free_split(char *argv[]);
-void	*free_everything(t_prog *p);
-void	*write_dead(t_philo *philo);
-void	prep_philos(t_prog *p, int i);
-int		arg_checker(int argc, char *argv[]);
-void	mutex_manager(t_prog *p, int action);
-void	write_message(t_philo *philo, char *msg, int eating);
+char				*ft_itoa(int n);
+int					ft_isdigit(int c);
+int					get_time(t_prog	*p);
+void				*lock(t_philo *philo);
+int					find_death(t_prog *p);
+void				*routine(void *philo_v);
+int					ft_strlen(const char *s);
+int					ft_atoi(const char *str);
+void				*build_params(t_prog *p);
+void				start_threads(t_prog *p);
+void				free_split(char *argv[]);
+void				*free_everything(t_prog *p);
+int					write_dead(t_philo *philo);
+void				prep_philos(t_prog *p, int i);
+int					arg_checker(int argc, char *argv[]);
+void				mutex_manager(t_prog *p, int action);
+char				*ft_strjoin(char const *s1, char const *s2);
+int					ft_strlcat(char *dst, const char *src, int dstsize);
+int					write_message(t_philo *philo, char *msg, int eating);
+unsigned long int	ft_strlcpy(char *dst, const char *src, \
+		unsigned long int dstsize);
 
 #endif
